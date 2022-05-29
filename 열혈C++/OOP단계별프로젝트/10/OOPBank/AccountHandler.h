6b3c4e5c556482dad431d3db8c956611b7faeee9
Leeinsol <63082755+Leeinsol@users.noreply.github.com>
2022-05-29 17:48:20 +0900

    Add files via upload

---
+++ 열혈C++/OOP단계별프로젝트/10/OOPBank/AccountHandler.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef __ACCOUNT_HANDLER_H__
+#define __ACCOUNT_HANDLER_H__
+
+#include "Account.h"
+#include "BoundCheckArray.h"
+
+class AccountHandler {
+private:
+	BoundCheckArray<Account*> accArr;
+	int accNum = 0;			//저장된 Account 수
+
+public:
+	AccountHandler();
+	void ShowMenu(void) const;		//메뉴출력
+	void MakeAccount(void);		//계좌개설
+	void DepositMoney(void);	//입금
+	void WithdrawMoney(void);	//출금
+	void ShowAllAccInfo(void) const;	//잔액조회
+	~AccountHandler();
+
+protected:
+	void MakeNormalAccount(void);
+	void MakeCreditAccount(void);
+
+};
+
+#endif // !__ACCOUNT_HANDLER_H__
+
